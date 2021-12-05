eb540e06ae6544bc370c6dbbff84492527e3ba3c
muflikhandimasd <muflikhan.dimas@gmail.com>
2021-12-06 06:39:29 +0700

    add internet permission android

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Qtasnim App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Qtasnim App'),
     );
   }
 }
@@ -35,204 +35,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _textFieldController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
-          title: Text('Qtasnim App'),
+          title: const Text('Qtasnim App'),
           backgroundColor: Colors.black,
           centerTitle: true,
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: Text('Tambah Data Penjualan'),
-                    content: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Nama Barang'),
-                            SizedBox(height: 4),
-                            TextField(
-                              cursorColor: Colors.black,
-                              decoration: InputDecoration(
-                                hintText: 'Buku',
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(9),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(
-                                    9,
-                                  ),
-                                  borderSide: BorderSide(
-                                    color: Colors.purple,
-                                  ),
-                                ),
-                              ),
-                              onChanged: (value) {
-                                setState(() {
-                                  valueText = value;
-                                });
-                              },
-                              controller: _textFieldController,
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text('Stok'),
-                            SizedBox(height: 4),
-                            TextField(
-                              cursorColor: Colors.black,
-                              decoration: InputDecoration(
-                                hintText: '100',
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(9),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(
-                                    9,
-                                  ),
-                                  borderSide: BorderSide(
-                                    color: Colors.purple,
-                                  ),
-                                ),
-                              ),
-                              onChanged: (value) {
-                                setState(() {
-                                  valueText = value;
-                                });
-                              },
-                              controller: _textFieldController,
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text('Jumlah Terjual'),
-                            SizedBox(height: 4),
-                            TextField(
-                              cursorColor: Colors.black,
-                              decoration: InputDecoration(
-                                hintText: '3000',
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(9),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(
-                                    9,
-                                  ),
-                                  borderSide: BorderSide(
-                                    color: Colors.purple,
-                                  ),
-                                ),
-                              ),
-                              onChanged: (value) {
-                                setState(() {
-                                  valueText = value;
-                                });
-                              },
-                              controller: _textFieldController,
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text('Tanggal Transaksi'),
-                            SizedBox(height: 4),
-                            TextField(
-                              cursorColor: Colors.black,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(9),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(
-                                    9,
-                                  ),
-                                  borderSide: BorderSide(
-                                    color: Colors.purple,
-                                  ),
-                                ),
-                              ),
-                              onChanged: (value) {
-                                setState(() {
-                                  valueText = value;
-                                });
-                              },
-                              controller: _textFieldController,
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text('Jenis Barang'),
-                            SizedBox(height: 4),
-                            TextField(
-                              cursorColor: Colors.black,
-                              decoration: InputDecoration(
-                                hintText: 'Konsumsi',
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(9),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(
-                                    9,
-                                  ),
-                                  borderSide: BorderSide(
-                                    color: Colors.purple,
-                                  ),
-                                ),
-                              ),
-                              onChanged: (value) {
-                                setState(() {
-                                  valueText = value;
-                                });
-                              },
-                              controller: _textFieldController,
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                    actions: <Widget>[
-                      TextButton(
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.red),
-                        ),
-                        child: Text('CANCEL',
-                            style: TextStyle(color: Colors.white)),
-                        onPressed: () {
-                          setState(() {
-                            Navigator.pop(context);
-                          });
-                        },
-                      ),
-                      TextButton(
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.green),
-                        ),
-                        child:
-                            Text('OK', style: TextStyle(color: Colors.white)),
-                        onPressed: () {
-                          setState(() {
-                            codeDialog = valueText;
-                            Navigator.pop(context);
-                          });
-                        },
-                      ),
-                    ],
-                  );
-                });
-          },
-          child: Icon(Icons.add),
-          backgroundColor: Colors.lightGreen,
         ),
         body: FutureBuilder(
           future: getDataPenjualanSource(),
@@ -243,13 +54,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     source: snapshot.data,
                     columns: getColumns(),
                     allowSorting: true,
-                    allowEditing: true,
-                    selectionMode: SelectionMode.single,
-                    navigationMode: GridNavigationMode.row,
                   )
                 : const Center(
                     child: CircularProgressIndicator(
-                      strokeWidth: 3,
+                      strokeWidth: 5,
                     ),
                   );
           },
